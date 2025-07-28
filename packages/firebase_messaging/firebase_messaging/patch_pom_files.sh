50e4f4eadd03e39f81d8697a314ccd37fc0091ab
Serhan Apaydın <zerhan@gmail.com>
2025-07-28 19:08:14 +0300

    Remove ktx-related things

--- packages/firebase_messaging/firebase_messaging/patch_pom_files.sh
+++ packages/firebase_messaging/firebase_messaging/patch_pom_files.sh
@@ -7,19 +7,12 @@ set -x
 target_version=$1
 target_version_suffixed=$target_version-defused
 path_to_packages="example/android/app/local_aars/com/google/firebase"
-package1_name=firebase-messaging
-pom1_name="$package1_name-$target_version_suffixed.pom"
-package2_name=firebase-messaging-ktx
-pom2_name="$package2_name-$target_version_suffixed.pom"
+package_name=firebase-messaging
+pom_name="$package_name-$target_version_suffixed.pom"
 pattern="<version>$target_version</version>"
 replacement="<version>$target_version_suffixed</version>"
 
-cd "$path_to_packages/$package1_name/$target_version_suffixed" &&
+cd "$path_to_packages/$package_name/$target_version_suffixed" &&
 curl "https://dl.google.com/dl/android/maven2/com/google/firebase/firebase-messaging/$target_version/firebase-messaging-$target_version.pom" \
-     >"$pom1_name" &&
-perl -0777 -pi -e "s@$pattern@$replacement@g" "$pom1_name" &&
-cd - &&
-cd "$path_to_packages/$package2_name/$target_version_suffixed" &&
-curl "https://dl.google.com/dl/android/maven2/com/google/firebase/firebase-messaging-ktx/$target_version/firebase-messaging-ktx-$target_version.pom" \
-     >"$pom2_name" &&
-perl -0777 -pi -e "s@$pattern@$replacement@g" "$pom2_name"
+     >"$pom_name" &&
+perl -0777 -pi -e "s@$pattern@$replacement@g" "$pom_name"
